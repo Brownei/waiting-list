80f8f5bbd5ae5c63e1613a34b17fcf57e6f141eb
Brownson <esitibrownson@gmail.com>
2024-03-24 15:58:22 +0100

    Could not notify people to be in a straight line

--- src/api/waiting-list.ts
+++ src/api/waiting-list.ts
@@ -1,18 +1,27 @@
 import express from 'express';
 import { prisma } from '../utils/db';
 import { Emailer } from '../utils/emailer';
+import { google } from 'googleapis';
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   const { name, email, country } = req.body;
-  const emailer = new Emailer(email, name);
 
   if (!name || !email || !country) {
     return res.sendStatus(404);
   }
 
   try {
+    const oAuth2Client = new google.auth.OAuth2(
+      process.env.GOOGLE_CLIENT_ID,
+      process.env.GOOGLE_CLIENT_SECRET,
+      process.env.GOOGLE_REDIRECT_URL,
+    );
+    
+    oAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
+    
+    const accessToken = await oAuth2Client.getAccessToken();
     const alreadyInTheWaitingList = await prisma.waitingList.findUnique({
       where: {
         email,
@@ -22,6 +31,7 @@ router.post('/', async (req, res) => {
     if (alreadyInTheWaitingList) {
       return res.sendStatus(409);
     } else {
+      const emailer = new Emailer(email, name, accessToken.token!);
 
       await prisma.waitingList.create({
         data: {
@@ -31,8 +41,8 @@ router.post('/', async (req, res) => {
         },
       });
   
-      emailer.notifyUserForJoiningWaitingList();
-      return res.sendStatus(201);
+      const mail = await emailer.notifyUserForJoiningWaitingList();
+      return mail === 'Mail sent' ? res.sendStatus(201) : res.sendStatus(409);
     }
 
   } catch (error) {
--- src/utils/emailer.ts
+++ src/utils/emailer.ts
@@ -7,9 +7,25 @@ config();
 
 const joinedTheWaitingListTemplate = (email: string, name: string): MailOptions => {
   return {
-    from: `Brownson From 4D Solutions, ${process.env.GMAIL_USER}`,
+    from: process.env.GMAIL_USER,
     to: email,
     subject: 'Welcome to the Waiting List!',
+    text: `
+      Dear ${name},
+      We are thrilled to welcome you to our exclusive waiting list community! 🎉
+      Thank you for expressing interest in LinT. Your decision to join our waiting list demonstrates your enthusiasm for a collaborative platform designed to empower tech enthusiasts like yourself to be able to enhance your skills, share knowledge, and advance your career.
+      While you await your turn, rest assured that you are part of an exciting journey towards growing your career and skills. We are continuously working to ensure that your experience with us exceeds your expectations.
+      As a member of our waiting list, you'll enjoy the following perks:
+      Priority access to LinT when it becomes available.
+      Exclusive updates and insights about our progress.
+      Opportunities to provide feedback and shape the future of LinT.
+      We appreciate your patience and trust in us. Please feel free to reach out if you have any questions or suggestions. We value your input and look forward to serving you.
+      In the meantime, keep an eye on your inbox for exciting updates and announcements. Great things are on the horizon!
+      Warm regards,
+      Brownson Esiti
+      Co-Founder
+      4D Solutions
+    `,
     html: `
       <p>Dear ${name},</p>
       <p>We are thrilled to welcome you to our exclusive waiting list community! 🎉</p>
@@ -28,40 +44,52 @@ const joinedTheWaitingListTemplate = (email: string, name: string): MailOptions 
   };
 };
 
+
 export class Emailer {
   private readonly transporter: nodemailer.Transporter;
-  email: string;
-  name: string;
+  private readonly email: string;
+  private readonly name: string;
+  private readonly access: string;
 
-  constructor(email: string, name: string) {
+  constructor(email: string, name: string, accessToken: string) {
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
+        type: 'OAuth2',
         user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASSWORD,
+        clientId: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+        accessToken: accessToken,
+      },
+      tls: {
+        rejectUnauthorized: true,
       },
     });
     this.email = email;
     this.name = name;
+    this.access = accessToken;
   }
 
   private async sendEmail(mailOptions: MailOptions) {
     try {
-      await this.transporter.sendMail(mailOptions);
-      // console.log(mail.messageId);
+      const mail = await this.transporter.sendMail(mailOptions);
       return {
+        id: mail.messageId,
         success: true,
       };
     } catch (error) {
       console.log(error);
       return {
+        id: null,
         success: false,
       };
     }
   }
 
-  public notifyUserForJoiningWaitingList() {
-    this.sendEmail(joinedTheWaitingListTemplate(this.email, this.name));
+  public async notifyUserForJoiningWaitingList() {
+    const sendMail = await this.sendEmail(joinedTheWaitingListTemplate(this.email, this.name));
+    return sendMail.id !== null ? 'Mail sent' : 'Mail not sent';
   }
 }
 
