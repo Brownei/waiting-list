83e1d982fcf094aaf5d01006512721f0d6490c36
Brownson <esitibrownson@gmail.com>
2024-04-11 14:19:29 +0100

    Frustration made me remove it bro!

--- src/api/waiting-list.ts
+++ src/api/waiting-list.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import { prisma } from '../utils/db';
-import { Emailer } from '../utils/emailer';
-import { google } from 'googleapis';
 
 const router = express.Router();
 
@@ -13,8 +11,6 @@ router.post('/', async (req, res) => {
   }
 
   try {
-    const OAuth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL);
-    OAuth2Client.setCredentials({ refresh_token: process.env.GOOGLE_REFRESH_TOKEN });
     const alreadyInTheWaitingList = await prisma.waitingList.findUnique({
       where: {
         email,
@@ -24,8 +20,6 @@ router.post('/', async (req, res) => {
     if (alreadyInTheWaitingList) {
       return res.sendStatus(400);
     } else {
-      const accessToken = await OAuth2Client.getAccessToken();
-      const emailer = new Emailer(email, name, accessToken.token!);
 
       await prisma.waitingList.create({
         data: {
@@ -34,9 +28,8 @@ router.post('/', async (req, res) => {
           country,
         },
       });
-  
-      const mail = await emailer.notifyUserForJoiningWaitingList();
-      return mail === 'Mail sent' ? res.sendStatus(201) : res.sendStatus(409);
+      
+      return res.sendStatus(201);
     }
 
   } catch (error) {
